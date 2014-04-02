7001b3bbae1336f114db09b93f5bb9f9d8e9172c
hollojeff <jhurst03@mail.bbk.ac.uk>
2014-04-02 21:53:34 +0100

    search.pl tidyup
    Removed spurious html, added in comments

--- search.pl
+++ search.pl
@@ -6,21 +6,32 @@ my $form = $cgi->param('form');
 my $search = $cgi->param('searchtype');
 my $return = $cgi->param('ReturnAll');
 
+
+
+#Clicked on Show all button
+#Return all results
 if ($return == "Show All"){
+
+#Call script to return all results
+
 print <<__BLAH;
 print $cgi->header();
 <html>
 <head>
-   <title>WHHHHHHHHHHHHHYYYYYYYY</title>
+   <title></title>
 </head>
 <body>
-<h1>WHHHHHHHHHHHHHHHHHHHHHYYYYYYYYYYYYYYYYYY</h1>
+<h1></h1>
 </body>
 </html>
 __BLAH
-
 }
+
+#Search using form and searchtype
 else {
+
+#Call script, sending over searchtype and form for SQL query
+
 print $cgi->header();
 print <<__BLAH;
 <html>
