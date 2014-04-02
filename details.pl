c396cd70422b5c70afff4e1eb0214cb0dda5419e
hollojeff <jhurst03@mail.bbk.ac.uk>
2014-04-02 22:42:31 +0100

    Add details.pl
    Put in skeleton script for retreiving the full details of a gene.

---
+++ details.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use CGI;
+use strict;
+my $cgi = new CGI;
+
+#Call module retrieving full details
+
+#return full 
+
+
+
+
+print <<__BLAH;
+print $cgi->header();
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+<h1></h1>
+</body>
+</html>
+__BLAH
+}
